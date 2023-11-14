13832616157ac54d9ab6e19405e4703f6cef9595
Aryan Roy <abr5813@psu.edu>
2023-11-14 14:13:50 -0500

    BoxScores

--- Phase2/csvWriter.py
+++ Phase2/csvWriter.py
@@ -5,24 +5,28 @@ import csv
 
 
 def csv_writer(soup,fileName,m):
-    table = soup.find('table')
+    tables = soup.find_all("table")
 
-    if table:
-        # Initialize lists to store the table data
-        data = []
-        for row in table.find_all('tr'):
-            row_data = [cell.get_text() for cell in row.find_all(['th', 'td'])]
-            data.append(row_data)
+    if tables:
+        for table in tables:
+            # Initialize lists to store the table data
+            data=[]
+            headers = [header.text for header in table.find('tr').find_all('th')]
+            data.append(headers)
+            rows = table.find_all('tr')[1:] 
+            for row in rows:
+                row_data = [cell.get_text() for cell in row.find_all(['th', 'td'])]
+                data.append(row_data)
 
-        # Define the CSV file name
-        
+            # Define the CSV file name
+            
+            
+            # Write the data to the CSV file
+            with open(fileName, m, newline='') as file:
+                writer = csv.writer(file)
+                for row_data in data:
+                    writer.writerow(row_data)
 
-        # Write the data to the CSV file
-        with open(fileName, m, newline='') as file:
-            writer = csv.writer(file)
-            for row_data in data:
-                writer.writerow(row_data)
-
-        print(f"Table data has been saved to {fileName}")
+            print(f"Table data has been saved to {fileName}")
     else:
         print("Desired table not found.")
