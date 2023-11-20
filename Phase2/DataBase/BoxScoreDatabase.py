f32a368dbe34d1db67ec8605b58d98ab945e72b5
Aryan Roy <abr5813@psu.edu>
2023-11-20 14:54:14 -0500

    Fixed the bug related to games going to OT

--- Phase2/DataBase/BoxScoreDatabase.py
+++ Phase2/DataBase/BoxScoreDatabase.py
@@ -6,21 +6,29 @@ import numpy as np
 
 
 # Connect to the SQLite database
-conn_Main,cursor_Main = conncect_db.connect_to_database('Main')
-conn_BoxScores,cursor_BoxScores = conncect_db.connect_to_database('BoxScores')
+conn,cursor = conncect_db.connect_to_database('Main')
+#conn_BoxScores,cursor_BoxScores = conncect_db.connect_to_database('BoxScores')
+"""cursor.execute('SELECT BoxScore_Id FROM BoxScores ORDER BY boxscores_id DESC LIMIT 1')
 
+latest_entry = cursor.fetchone()
+latest_year = latest_entry[0:4]
+latest_month = latest_entry[4:6]
+latest_date = latest_entry[6:8]
+
+"""
 # Execute a SELECT query to retrieve all values in the 'Token' column
-cursor_Main.execute('SELECT Token FROM schedule WHERE Winner IS NOT NULL')
+cursor.execute('SELECT Token,OT FROM schedule WHERE Winner IS NOT NULL')
 
 # Fetch all the results
-tokens = cursor_Main.fetchall()
-conncect_db.close_database_connection(conn_Main)
+tokens = cursor.fetchall()
+#conncect_db.close_database_connection(conn)
 # Iterate through the tokens
-"""
-conn_BoxScores.execute(f'DROP TABLE IF EXISTS BoxScores')
-cursor_BoxScores.execute(f'''
+
+conn.execute(f'DROP TABLE IF EXISTS BoxScores')
+cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS BoxScores (
-        boxscore_id VARCHAR(255) PRIMARY KEY,
+        boxscores_id INTEGER PRIMARY KEY,
+        "BoxScore_ID" TEXT,
         "Player" TEXT,
         "MP" TEXT,
         "FG" REAL,
@@ -31,7 +39,7 @@ cursor_BoxScores.execute(f'''
         "3P%" REAL,
         "FT" REAL,
         "FTA" REAL,
-        "FT%" REAL
+        "FT%" REAL,
         "ORB" REAL,
         "DRB" REAL,
         "TRB" REAL,
@@ -41,13 +49,14 @@ cursor_BoxScores.execute(f'''
         "TOV" REAL,
         "PF" REAL,
         "PTS" REAL,
-        "+/-" TEXT
+        "+/-" TEXT,
+        "BoxScore_Type" TEXT
         
     )
 ''')
 
-conn_BoxScores.commit()
-"""
+conn.commit()
+
 for t in tokens:
     tok = t[0]
     
@@ -65,13 +74,17 @@ for t in tokens:
     home=tok[9:12]
     away=tok[-3:]
     
-    header_titles=["G","Q1","Q2","H1","Q3","Q4","H2"]
+    if t[1] == "OT":
+        header_titles=["G","Q1","Q2","H1","Q3","Q4","H2","OT","AG"]
+    else:
+        header_titles=["G","Q1","Q2","H1","Q3","Q4","H2","AG"]
+
     ind=0
     flag=away
     # Iterate through rows
     for index, row in box_df.iterrows():
-    
-        if 'Basic Box Score Stats' in row[1]:
+        
+        if type(row[1]) != float and 'Basic Box Score Stats' in row[1]:
             # If a new header is found, save the current dataframe (if any)
             # and create a new one with the new header
             if current_header is not None:
@@ -79,30 +92,47 @@ for t in tokens:
             if ind==len(header_titles):
                 ind = 0
                 flag = home 
-            current_header = header_titles[ind]+"_"+flag
+            current_header = header_titles[ind]+" "+flag
             
             ind+=1
             current_dataframe = pd.DataFrame(columns=box_df.columns)
-        else:
             # Append the current row to the current dataframe
+        elif type(row[1]) != float and 'Advanced Box Score Stats' in row[1]:
+            if current_header is not None:
+                dataframes[current_header] = current_dataframe
+            current_header = header_titles[ind]+" "+flag 
+            ind+=1
+            current_dataframe = pd.DataFrame(columns=box_df.columns)
+            
+        else:    
             current_dataframe = current_dataframe.append(pd.Series(row, index=box_df.columns), ignore_index=True)
 
     # Save the last dataframe
     if current_header is not None:
         if current_header not in dataframes:
             dataframes[current_header] = current_dataframe
-    
+        
+    keys_to_delete = [key for key in dataframes.keys() if 'AG' in key]
+
+    for key in keys_to_delete:
+        del dataframes[key]
 
     for k in dataframes:
-        new_column_names = dataframes[k].iloc[0]
+
+        new_column_names = (dataframes[k].iloc[0]).tolist()
         new_column_names[0] = "Player"
+
+        new_column_names.append("BoxScore_ID")
+        new_column_names.append("BoxScore_Type")
         mask = dataframes[k][2]!= 'FG'
         df = dataframes[k][mask]
+        df["BoxScore_ID"] = tok+"_"+k
+        df["BoxScore_Type"] = k[0] if k[0] == 'G' else k[:2]
         df.rename(columns=dict(zip(df.columns, new_column_names)), inplace=True)
         
-        df.to_sql(f'Box{tok}_{k}', conn_BoxScores, if_exists='replace', index=False)
+        df.to_sql(f'BoxScores', conn, if_exists='append', index=False)
 
-conncect_db.close_database_connection(conn_BoxScores)
+conncect_db.close_database_connection(conn)
 
 
 																			
